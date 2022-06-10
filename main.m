55345417ded6517278b4e73838ce7869842a67e9
Sergey Litvinov <slitvinov@gmail.com>
2022-06-10 14:52:07 -0400

    up

--- main.m
+++ main.m
@@ -11,33 +11,37 @@ k = [0:n/2-1 -n/2:-1];
 k1 = 1i*k;
 k2 = k1.^2;
 
-U = 1/sqrt(3) * (2 * pi)^(1/4) * k .^2 .* exp(-k.^2 / 4);
+U = k .^2 .* exp(-k.^2 / L);
 a =  fft(randn(1, n));
 uinit = ifft(U .* a);
-uinit = uinit / max(abs(uinit));
+uinit = 0.8 * uinit / max(abs(uinit));
 u0 = uinit;
 E0 = sumsq(u0) * h;
-
 q = 0;
 figure('visible', 'off');
 for t = 0:dt:2000
     E = sumsq(u0) * h;
-
     force = fft(u0);
-    force(~(abs(k) <= 2)) = 0;
-    force = 0.1 * force .* (E0 - E) / dt;
-    
-    u1hat = fft(u0) + 0.5*dt*(-k1.*fft(u0.^2)/2 + mu*k2.*fft(u0) + force);
+    force(abs(k) >= 3) = 0;
+    force .*= (E0 - E) / dt;
+    rhs = @(u) (-k1.*fft(u0.^2)/2 + mu*k2.*fft(u0) + force);
+    u1hat = fft(u0) + 0.5*dt*rhs(u0);
     u1 = ifft(u1hat);
-    u2hat = fft(u0) + dt*(-k1.*fft(u1.^2)/2 + mu*k2.*fft(u1) + force);
+    u2hat = fft(u0) + dt*rhs(u1);
+    u0 = ifft(u2hat);
     if mod(q, 5000) == 0
+      uf = u2hat;
+      uf(abs(k) >= 70) = 0;
+      uf = ifft(uf);
       force = ifft(force);
-      plot(x, u0, 'color', 'black', 'linewidth', 5, x, force/max(abs(force)), 'color', 'black')
+      force /= max(abs(force));
+      plot(x, u0, 'color', 'black', 'linewidth', 4,
+	   x, uf, 'color', 'black', 'linewidth', 1,
+           x, force, 'color', 'black', 'linewidth', 1)
       axis([0,2*pi,-1, 1])
       title(sprintf('%12.2f', t))
       saveas(gcf(), sprintf('u.%09d.png', q))
       printf('%.16e\n', E);
     end
-    u0 = ifft(u2hat);
     q += 1;
 end
